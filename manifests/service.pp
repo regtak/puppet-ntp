7818d0c3bddcb088044435e03badd41551df2a93
regtak <maksim.shayakhmetov@gmail.com>
2017-09-01 03:56:58 +0000

    1st one

---
+++ manifests/service.pp
@@ -0,0 +1,16 @@
+class ntp::service(
+  String $service_ensure      = $ntp::service_ensure,
+  String $service_name        = $ntp::service_name,
+  Boolean $service_enable     = $ntp::service_enable,
+  Boolean $service_hasstatus  = $ntp::service_hassstatus,
+  Boolean $service_hasrestart = $ntp::service_hasrestart,
+) {
+  service {'ntp':
+    ensure     => $service_ensure,
+    enable     => $service_enable,
+    name       => $service_name, 
+    status     => $service_hasstatus,
+    hasrestart => $service_hasrestart,
+  }
+
+}
